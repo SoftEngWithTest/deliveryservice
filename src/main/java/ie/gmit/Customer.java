c65f945876a408598913640923ca5c9360e4acf8
Ruairi <g00352092@gmit.ie>
2020-05-07 21:03:44 +0100

    Tests for Customer class complete

--- src/main/java/ie/gmit/Customer.java
+++ src/main/java/ie/gmit/Customer.java
@@ -1,5 +1,8 @@
 package ie.gmit;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Customer {
     private String firstName;
     private String lastName;
@@ -18,35 +21,86 @@ public class Customer {
     }
 
     public Customer(String firstName, String lastName, String email, String address, String county, String eircode) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.address = address;
-        this.county = county;
-        this.eircode = eircode;
+        setFirstName(firstName);
+        setLastName(lastName);
+        setEmail(email);
+        setAddress(address);
+        setCounty(county);
+        setEircode(eircode);
     }
 
     public String getFirstName() { return firstName; }
 
-    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setFirstName(String firstName) {
+        if(firstName.length() < 2)
+            throw new IllegalArgumentException("First name must be at least 2 characters");
+        else
+            this.firstName = firstName;
+    }
 
     public String getLastName() { return lastName; }
 
-    public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setLastName(String lastName) {
+        if(lastName.length() < 2)
+            throw new IllegalArgumentException("Last name must be at least 2 characters");
+        else
+            this.lastName = lastName;
+    }
 
     public String getEmail() { return email; }
 
-    public void setEmail(String email) { this.email = email; }
+    public void setEmail(String email) {
+        if(email.length() < 10)
+            throw new IllegalArgumentException("Email must be at least 10 characters");
+        else
+            this.email = email;
+    }
 
     public String getAddress() { return address; }
 
-    public void setAddress(String address) { this.address = address; }
+    public void setAddress(String address) {
+        if(address.length() < 5)
+            throw new IllegalArgumentException("Address must be at least 5 characters");
+        else
+            this.address = address;
+    }
 
     public String getCounty() { return county; }
 
-    public void setCounty(String county) { this.county = county; }
+    public void setCounty(String county) {
+        if(county.length() < 4)
+            throw new IllegalArgumentException("County must be at least 4 characters");
+        else
+            this.county = county;
+    }
 
     public String getEircode() { return eircode; }
 
-    public void setEircode(String eircode) { this.eircode = eircode; }
+    public void setEircode(String eircode) {
+        //Using Regular Expressions to make sure correct Eircode format was used
+        //String regex is the correct format and will use this to compare to user entry
+        String regex = "\\D\\d\\d\\s\\D\\d\\D\\d";
+
+        Pattern pt = Pattern.compile(regex);
+        Matcher m = pt.matcher(eircode);
+        boolean result = m.matches();
+
+        if(eircode.length() != 8)
+            throw new IllegalArgumentException("Eircode must be 8 characters");
+        else if(result == false)
+            throw new IllegalArgumentException("Eircode must follow the correct format e.g H78 J9K6");
+        else
+            this.eircode = eircode;
+
+
+
+
+        //if entered correctly
+        if(result) {
+            System.out.println("Eircode entered correctly");
+        }
+        else {
+            System.out.println("Eircode entered incorrectly");
+        }
+    }
 }
