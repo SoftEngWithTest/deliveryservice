54e8f1def02feb56766936eebbd28378b3d70c37
Ruairi <g00352092@gmit.ie>
2020-05-07 15:04:09 +0100

    Delete dummyTest & dummyClass

--- src/main/java/ie/gmit/GenerateLabel.java
+++
@@ -1,8 +0,0 @@
-/* This class generates a JSON label containing customer details
-   plus a unique order ID
- */
-
-package ie.gmit;
-
-public class GenerateLabel {
-}
--- src/main/java/ie/gmit/JSONParse.java
+++ src/main/java/ie/gmit/JSONParse.java
@@ -4,6 +4,8 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
+import java.awt.*;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
