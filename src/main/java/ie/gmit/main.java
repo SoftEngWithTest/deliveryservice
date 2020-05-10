ca9c8a3113d5a14cb81859c3eaab6ef7021a6a2a
Arvids <arvid141088@gmail.com>
2020-05-10 13:14:58 +0100

    Adding parcel dispatch/receive

--- src/main/java/ie/gmit/main.java
+++ src/main/java/ie/gmit/main.java
@@ -1,9 +1,27 @@
 package ie.gmit;
 
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
         JSONParse parser = new JSONParse();
 
         parser.readJSON();
+
+        //switch case
+        Driver driver = new Driver(Label.orderId,"888888","John");
+        ParcelDispatch parcelDispatch = new ParcelDispatch();
+        parcelDispatch.registerParcelReadyToDispatch();
+        parcelDispatch.searchOrder("111222");
+        parcelDispatch.searchOrder(driver.getOrderId());
+        parcelDispatch.registerParcelDispatched(driver);
+        System.out.println("Driver name: "+driver.getDriverName()+"\nDriver ID: "+driver.getDriverId()+"\nOrder ID: "+
+                driver.getOrderId()+"\nLabel: "+Label.myList.get(Label.myList.size()-1));
+
+        //Line below for demo purpose
+        System.out.println(driver.getOrderId());
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ENTER order ID : ");
+        parcelDispatch.searchDispatched(scanner.nextLine());
     }
 }
